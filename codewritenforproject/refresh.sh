f86547329046b49451a2ff98b6432ede12a51fb9
root <bennett.235@wright.edu>
2025-05-01 12:13:57 -0400

    forgot to expose port

--- codewritenforproject/refresh.sh
+++ codewritenforproject/refresh.sh
@@ -2,4 +2,4 @@
 sudo docker kill app
 sudo docker rm app
 sudo docker pull bennett5027/bennett-ceg3120:latest
-sudo docker run -d --name app bennett5027/bennett-ceg3120:latest
+sudo docker run -d --name app -p 80:4200 bennett5027/bennett-ceg3120:latest
